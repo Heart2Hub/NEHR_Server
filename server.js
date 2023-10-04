2d5abe5d14344a9bc253c48c9dd6edcaffab139a
ernestchan <ernest.chan@u.nus.edu>
2023-10-04 20:58:26 +0800

    Resolved Timestamp Issue When Creating Patients

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const { PORT_NUM } = require("./constants/PortNum");
 
 const jsonServer = require("json-server");
@@ -8,6 +9,19 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  // Iterate through the request body and update date-time fields
+  if (req.method === "POST" || req.method === "PUT") {
+    for (const key in req.body) {
+      if (typeof req.body[key] === "string" && moment(req.body[key], moment.ISO_8601, true).isValid()) {
+        // Check if the field is a valid date-time in ISO 8601 format
+        req.body[key] = moment(req.body[key]).format("YYYY-MM-DD HH:mm:ss");
+      }
+    }
+  }
+  next();
+});
 
 server.get("/records/:nric", (req, res) => {
   const nricToFind = req.params.nric;
@@ -35,9 +49,9 @@ server.post("/records", (req, res) => {
     });
   }
 
-  records.push(newRecord);
+  // records.push(newRecord);
 
-  router.db.set("records", records).write();
+  // router.db.set("records", records).write();
 
   res.status(200).json(newRecord);
 });
