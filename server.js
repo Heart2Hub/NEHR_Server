99288ae9725eccbc682abcfa38563e5132402304
ernestchan <ernest.chan@u.nus.edu>
2023-10-10 16:14:07 +0800

    Uncomment write into EHR DB

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ server.post("/records", (req, res) => {
     });
   }
 
-  // records.push(newRecord);
+  records.push(newRecord);
 
-  // router.db.set("records", records).write();
+  router.db.set("records", records).write();
 
   res.status(200).json(newRecord);
 });
